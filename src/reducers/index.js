aa1fd1b875106bcf0ad73a2f0792357e8e916f8d
Kajal-Abacus <73093240+Kajal-Abacus@users.noreply.github.com>
2022-10-14 18:58:37 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,17 @@
-const initialState = {
-    counter: 0
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from "../actions/action"
+function App() {
+  const store = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div id='main'>
+      <h3 data-testid='counter'>{store.counter}</h3>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
 }
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return { counter: state.counter + 1 };
-        case 'DECREMENT':
-            return { counter: state.counter - 1 };
-    }
-}
-
-export default reducer;
+export default App;
