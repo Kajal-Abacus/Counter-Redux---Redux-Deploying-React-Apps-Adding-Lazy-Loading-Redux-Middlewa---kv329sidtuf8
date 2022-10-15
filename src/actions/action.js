d611f4fda24407601c2bd5fe1dc86814c5ce4941
Kajal-Abacus <73093240+Kajal-Abacus@users.noreply.github.com>
2022-10-15 13:37:56 +0530

    Update action.js

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,2 +1,22 @@
-export const increment = () => ({ type: 'INCREMENT' });
-export const decrement = () => ({ type: 'DECREMENT' });
+const increment = () => ({
+    type: "INCREMENT"
+});
+
+const decrement = () => ({
+    type: "DECREMENT"
+});
+
+const signIn = () => ({
+    type: "SIGN_IN"
+});
+
+const signOut = () => ({
+    type: "SIGN_OUT"
+});
+
+const addAmount = (amount) => ({
+    type: "INCREMENTBYAMOUNT",
+    payload: amount
+});
+
+export { increment, decrement, addAmount, signIn, signOut };
