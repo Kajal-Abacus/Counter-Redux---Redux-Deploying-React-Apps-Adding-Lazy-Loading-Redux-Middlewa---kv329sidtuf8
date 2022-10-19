97d116e635210a0d2616a60b9d0118a278132e3d
Kajal-Abacus <73093240+Kajal-Abacus@users.noreply.github.com>
2022-10-19 11:52:14 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,50 +1,14 @@
 const initialState = {
-    counter: 0,
-    islogged: false
-};
+    counter: 0
+}
 
 const reducer = (state = initialState, action) => {
-    
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                ...state,
-                counter: state.counter + 1
-            };
-
+            return { counter: state.counter + 1 };
         case 'DECREMENT':
-            return {
-                ...state,
-                counter: state.counter - 1
-            };
-
-        case 'islogged': 
-            return {
-                ...state,
-                islogged: !state.islogged
-            };
-
-        case 'SIGN_IN': 
-            return {
-                ...state,
-                islogged: true
-            };
-
-        case 'SIGN_OUT': 
-            return {
-                ...state,
-                islogged: false
-            };
-            
-        case 'INCREMENTBYAMOUNT':
-            return {
-                ...state,
-                counter: action.payload
-            };
-
-        default:
-            return state
+            return { counter: state.counter - 1 };
     }
-};
+}
 
 export default reducer;
